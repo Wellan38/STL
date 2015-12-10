b6539883c4e339d6178fd9b70808fe1138e7243e
patrick <patrick.finnerty@insa-lyon.fr>
2015-12-10 10:50:24 +0100

    Updated compile std configurations.

--- TestMain/subdir.mk
+++ TestMain/subdir.mk
@@ -23,7 +23,7 @@ CPP_DEPS += \
 %.o: ../%.cpp
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C++ Compiler'
-	g++ -DTEST -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
+	g++ -std=c++0x -DTEST -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
