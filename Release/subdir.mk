b552c293c043e9c0ae5709b0ec2abab2a3230d88
patrick <patrick.finnerty@insa-lyon.fr>
2015-12-13 15:58:37 +0100

    Added release build

---
+++ Release/subdir.mk
@@ -0,0 +1,30 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CPP_SRCS += \
+../Analyser.cpp \
+../Launcher.cpp \
+../Log.cpp 
+
+OBJS += \
+./Analyser.o \
+./Launcher.o \
+./Log.o 
+
+CPP_DEPS += \
+./Analyser.d \
+./Launcher.d \
+./Log.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+%.o: ../%.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C++ Compiler'
+	g++ -std=c++0x -O3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
